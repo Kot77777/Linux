bcc4f25636527c818ab2cd98fa7b34b3fbbf33d7
Танов Константин <tanov.konstanti@yandex.ru>
2024-03-28 23:06:23 +0300

    new

--- Laboratory_work_2/save.sh
+++
@@ -1,6 +0,0 @@
-while true
-do
-        rsync -ab /mnt/C/user/tanov/SidMeiersCivilizationVI/Base/Assets/Benchmark/Saves  /mnt/C/Users/Mr\_Eclipse/OneDrive/
-        sleep 60
-done
-
---
+++ Laboratory_work_2/save/save.sh
@@ -0,0 +1,6 @@
+while true
+do
+	rsync -ab /mnt/c/Users/tanov/AppData/Roaming/\.minecraft/saves/iji/  /mnt/c/Users/tanov/Repositories/Linux/Laboratory\_work\_2/save/
+        sleep 60
+done
+
